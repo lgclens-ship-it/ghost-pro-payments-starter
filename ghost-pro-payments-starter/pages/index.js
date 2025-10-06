b48e3747a5fd128e4a881494bd82eeffa7f0efbc
lgclens-ship-it <lgclens@gmail.com>
2025-10-05 17:45:24 -0700

    index.js

---
+++ ghost-pro-payments-starter/pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main style={{minHeight:"60vh",display:"grid",placeItems:"center",fontFamily:"system-ui"}}>
+      <div>
+        <h1 style={{margin:0}}>Ghost Pro Payments Starter</h1>
+        <p style={{opacity:.7}}>APIs listas: /api/provision, /api/mp/preference, /api/webhooks/*</p>
+      </div>
+    </main>
+  );
+}
